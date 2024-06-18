ee9f211b2ad9c5ae5d9d45b1939385c15b0c2405
dinithi <dinithisamindika@gmail.com>
2024-06-19 00:27:22 +0100

    Data Preprocessing
    Remove Duplicates, Drop missing values, convert Date time into proper format and merge data frames

--- src/Guest_segmentation.py
+++ src/Guest_segmentation.py
@@ -31,3 +31,28 @@ sql_2 ="SELECT booking_no as booking_nu,guest_id as guest_id, type as type,room_
 #Execute SQL Queries
 df_reservations = pd.read_sql(sql_1,db_connection)
 df_expenses = pd.read_sql(sql_2,db_connection)
+
+#Remove Duplicates
+df_reservations = df_reservations.drop_duplicates()
+df_expenses = df_expenses.drop_duplicates()
+
+#Merge resesrvations and expenses dataframes
+df_guest_booking = pd.merge(df_reservations, df_expenses, on=['booking_nu', 'guest_id','date'], how='inner')
+
+#Drop missing values
+df_guest_booking = df_guest_booking.dropna()
+
+#convert date column to date time format
+df_guest_bookings = df_guest_booking.copy()
+df_guest_bookings['date'] = pd.to_datetime(df_guest_booking['date'])
+
+
+
+
+
+
+
+
+
+
+
