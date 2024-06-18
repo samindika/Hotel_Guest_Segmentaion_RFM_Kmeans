37f9a55677e05bc5713cad155917bc1f39fbc36a
dinithi <dinithisamindika@gmail.com>
2024-06-19 00:20:35 +0100

    Import Libraries
    Import necessary Libraries
    pandas, matplotlib, numpy, datetime, mysql_connector

--- src/Guest_segmentation.py
+++ src/Guest_segmentation.py
@@ -5,3 +5,13 @@ Created on Tue Jun 18 23:41:22 2024
 @author: dinit
 """
 
+#Import mysql connector
+import mysql.connector
+
+#import libraries 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+
