cd1355bb570c6e924d4f6da6834543a1c4597443
dinithi <dinithisamindika@gmail.com>
2024-06-19 00:22:44 +0100

    Execute SQL Queries
    Execute SQL Queries
    -expenses
    -reservations

--- src/Guest_segmentation.py
+++ src/Guest_segmentation.py
@@ -22,3 +22,12 @@ db_connection = mysql.connector.connect(
     password='rootpass',
     database='pms_system_bluewaters'
 )
+
+#SQL Queries
+# expenses and reservations tables
+sql_1 ="SELECT booking_no as booking_nu, room_id as room_id,date as date,guest_id as guest_id from reservations";
+sql_2 ="SELECT booking_no as booking_nu,guest_id as guest_id, type as type,room_charge as room_charge,net as net_amount,gross as gross_amount, date as date from expences";
+
+#Execute SQL Queries
+df_reservations = pd.read_sql(sql_1,db_connection)
+df_expenses = pd.read_sql(sql_2,db_connection)
